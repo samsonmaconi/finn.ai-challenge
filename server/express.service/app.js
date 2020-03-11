61cef4243e1f64d3691ae585294b0954e86cbb8b
Samson Maconi <git@samsonmaconi.com>
2020-03-11 00:24:23 -0700

    Setup initialization for persisted user storage

--- server/express.service/app.js
+++ server/express.service/app.js
@@ -6,7 +6,16 @@ const logger = require('morgan');
 const idsRouter = require('./routes/ids');
 const usersRouter = require('./routes/users');
 
+
+const User = require('./models/persistedUser');
+const {v4} = require('uuid');
+
 const app = express();
+
+// initialize datastore with initialUser info
+let storage = app.locals
+let initialUser = new User(v4(), 'Samson', 'Maconi', 'Solutions provider');
+initialUser.saveToStorage(storage)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
