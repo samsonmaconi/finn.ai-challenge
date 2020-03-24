88c21b883b67fbfc2724546fc137e77d6574e227
Samson Maconi <git@samsonmaconi.com>
2020-03-24 05:24:00 -0700

    bug fix

--- client/public/javascripts/script.js
+++ client/public/javascripts/script.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", async function () {
     try {
 
-        let response = await fetch(`http://web_server:3000/user`, { method: 'POST' })
+        let response = await fetch(`http://${location.hostname}:3000/user`, { method: 'POST' })
         console.log('response :', response);
         let { status } = response
         if (status == 201) {
